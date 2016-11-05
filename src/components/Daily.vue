60dfbe9e66ffcb88fad0d32c7e12f71cfdd1d77d
Constantin Schomburg <me@cschomburg.com>
2016-11-05 17:40:09 +0100

    Daily: experimental event timeline.

--- src/components/Daily.vue
+++ src/components/Daily.vue
@@ -1,23 +1,34 @@
 <template>
   <h1>Daily Overview for {{ date }}</h1>
+  <svg v-el:chart class="timeline"></svg>
   <div class="pure-g">
     <div class="pure-u-24-24">
       <h2>Events</h2>
-      <input type="text" v-model="eventFilter" placeholder="Filter events ..." />
-      <ul>
-        <li v-for="e in events | filterBy eventFilter | orderBy 'time' -1">
-          [ {{ e.time }} ]
-          <span class="danger">{{ e.action }}</span>
-          {{ e.text }}
-          <a class="debug inspect" v-on:click="$dispatch('inspect', e)"><i class="fa fa-gear"></i></a>
-        </li>
-      </ul>
+      <pre class="code"><code>{{ events | jsonFormat }}</code></pre>
     </div>
   </div>
 </template>
 
+<style>
+.timeline {
+  font-size: 10px;
+}
+
+.timeline .axis path,
+.timeline .axis line {
+  fill: none;
+  stroke: #000;
+  shape-rendering: crispEdges;
+}
+.timeline .dot {
+  opacity: 0.5;
+}
+</style>
+
 <script>
 import Sarif from '../sarif/service'
+import util from '../util'
+import d3 from 'd3'
 
 export default {
   data () {
@@ -40,15 +51,71 @@ export default {
       var dateEnd = new Date(dateStart.getTime())
       dateEnd.setHours(28)
 
-      Sarif.request({
+      Sarif.client.request({
         action: 'event/list',
         p: {
-          after: dateStart.toISOString(),
-          before: dateEnd.toISOString()
+          'time >': dateStart.toISOString(),
+          'time <': dateEnd.toISOString()
         }
       }, (reply) => {
         this.events = reply.p.events
+        this.drawTimeline(dateStart, dateEnd, this.events)
       })
+    },
+
+    drawTimeline (dateStart, dateEnd, events) {
+      var aggregated = util.aggregateMessages(events)
+      var data = d3.entries(aggregated)
+
+      var margin = {top: 20, right: 20, bottom: 30, left: 200}
+      var width = 960 - margin.left - margin.right
+      var height = 500 - margin.top - margin.bottom
+
+      var x = d3.time.scale().range([0, width]).domain([dateStart, dateEnd])
+      var y = d3.scale.ordinal().domain(d3.keys(aggregated)).rangePoints([height, 0], 0.5)
+
+      var xAxis = d3.svg.axis().scale(x).orient('bottom')
+      var yAxis = d3.svg.axis().scale(y).orient('left')
+
+      var svg = d3.select(this.$els.chart)
+          .attr('width', width + margin.left + margin.right)
+          .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+          .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+
+      svg.append('g')
+          .attr('class', 'x axis')
+          .attr('transform', 'translate(0,' + height + ')')
+          .call(xAxis)
+      svg.append('g')
+          .attr('class', 'y axis')
+          .call(yAxis)
+
+      svg.selectAll('.x.grid')
+          .data(d3.merge([[new Date()], x.ticks()]))
+        .enter().append('line')
+          .attr('class', 'x grid')
+          .attr('x1', function (d) { return x(d) })
+          .attr('y1', 0)
+          .attr('x2', function (d) { return x(d) })
+          .attr('y2', height)
+          .style('stroke-width', 1)
+          .style('stroke', function (d, i) { return (i > 0 ? '#eee' : '#f00') })
+
+      var series = svg.selectAll('.series')
+          .data(data)
+        .enter().append('g')
+          .attr('class', 'series')
+          .attr('transform', function (d) { return 'translate(0,' + y(d.key) + ')' })
+
+      series.selectAll('.dot')
+          .data(function (d) { return d.value })
+        .enter().append('circle')
+          .attr('class', 'dot')
+          .attr('r', 3.5)
+          .attr('cx', function (d) { return x(new Date(d.time)) })
+          .attr('cy', 0)
+          .style('fill', 'red')
     }
   }
 }
---
+++ src/util.js
@@ -0,0 +1,44 @@
+export default {
+  aggregateMessages (msgs) {
+    var prefixCount = {}
+
+    for (var i = 0; i < msgs.length; i++) {
+      var msg = msgs[i]
+      if (!msg.action) {
+        continue
+      }
+
+      var parts = msg.action.split('/')
+      var action = ''
+      for (var j = 0; j < parts.length; j++) {
+        action += (j > 0 ? '/' : '') + parts[j]
+        prefixCount[action] = (prefixCount[action] || 0) + 1
+      }
+    }
+
+    var byPrefix = {}
+    for (i = 0; i < msgs.length; i++) {
+      msg = msgs[i]
+      if (!msg.action) {
+        continue
+      }
+
+      parts = msg.action.split('/')
+      action = ''
+      for (j = 0; j < parts.length; j++) {
+        var suffix = (j > 0 ? '/' : '') + parts[j]
+        if (prefixCount[action + suffix] === 1) {
+          break
+        }
+        action += suffix
+      }
+      if (action === '') {
+        continue
+      }
+      byPrefix[action] = byPrefix[action] || []
+      byPrefix[action].push(msg)
+    }
+
+    return byPrefix
+  }
+}
