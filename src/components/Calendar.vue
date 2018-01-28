890e6a735f531b91759f6d1b2e6bfa09d4730e37
Constantin Schomburg <me@cschomburg.com>
2018-01-28 15:21:43 +0100

    New journal list for the calendar view.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,15 +2,41 @@
   <div>
     <h1>Calendar</h1>
     <div id="calendar-heatmap"></div>
-    <div class="daily-report" v-if="selectedReport">
-      <h2>Day {{selectedReport.time | date}}</h2>
-      <ul>
-        <li v-for="(val, key) in selectedReport">
-          <b>{{key}}:</b> {{val}}
-        </li>
-      </ul>
-      <pre>{{selectedReport | jsonFormat}}</pre>
-    </div>
+
+      <div class="pure-g">
+        <div class="pure-u-16-24">
+          <h2>Last entries</h2>
+          <div v-for="entry in lastEntries">
+            <a v-on:click="select(entry.time)">
+              <b>{{entry.time | date('ddd, l')}}</b>
+            </a>
+
+            <span v-for="tag in entry.review.tags" class="badge">
+              {{tag}}
+            </span>
+
+            <p>{{entry.review.summary}}</p>
+            <ul class="tag-bar">
+              <li>Mood: {{entry.review.mood_score}}</li>
+              <li>Browsing: {{Math.round(entry.browsing.online*10)/10}}</li>
+              <li>URLs: {{entry.browsing.urls}}</li>
+            </ul>
+            <hr>
+          </div>
+        </div>
+
+        <div class="pure-u-8-24">
+          <div class="daily-report" v-if="selectedReport">
+            <h2>Day {{selectedReport.time | date}}</h2>
+            <ul>
+              <li v-for="(val, key) in selectedReport">
+                <b>{{key}}:</b> {{val}}
+              </li>
+            </ul>
+            <pre>{{selectedReport | jsonFormat}}</pre>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -51,6 +77,26 @@ rect.day-cell:hover {
 .calendar-heatmap {
   box-sizing: initial;
 }
+
+.badge {
+  background: #5cc1df;
+  border-radius: 10px;
+  padding: 0px 6px;
+  color: #000;
+  margin-left: 5px;
+  font-size: 0.9em;
+}
+
+.tag-bar {
+  display: flex;
+}
+.tag-bar li {
+  flex-grow: 1;
+  list-style-type: none;
+  padding: 0px 5px;
+  font-weight: bold;
+  font-size: 0.8em;
+}
 </style>
 
 <script>
@@ -63,7 +109,8 @@ export default {
   data () {
     return {
       data: {},
-      selectedReport: null
+      selectedReport: null,
+      lastEntries: []
     }
   },
 
@@ -83,6 +130,7 @@ export default {
           limit: 365
         }
       }, (reply) => {
+        this.lastEntries = reply.p.slice(0, 14)
         reply.p.map(report => {
           var d = moment(report.time).format('YYYY-MM-DD')
           this.data[d] = report
@@ -106,13 +154,13 @@ export default {
         .tooltipEnabled(true)
         .colorRange(['#1f3033', '#eff'])
         .onClick((data) => {
-          this.select(data)
+          this.select(data.date)
         })
       heatmap()
     },
 
-    select (data) {
-      var d = moment(data.date).format('YYYY-MM-DD')
+    select (date) {
+      var d = moment(date).format('YYYY-MM-DD')
       this.selectedReport = util.flattenObject(this.data[d])
     }
   }
